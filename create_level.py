222d3239ce20c6eb7779b6e608e8105c7634f098
Rishat <lonelyotter100@gmail.com>
2020-02-12 17:55:27 +0300

    v1.1
    added new enemies

---
+++ create_level.py
@@ -0,0 +1,43 @@
+from random import randint as r
+import os
+
+FIELD_SIZE = 19
+
+
+#  Функция создания уровня
+def create_level():
+    field = [["#" if r(0, 1) else "." for _ in range(FIELD_SIZE)] for _ in range(FIELD_SIZE)]
+
+    for elem in field:
+        for k in range(len(elem)):
+            if not r(0, 8):
+                number = r(1, 3)
+                if number == 1:
+                    elem[k] = "E"
+                elif number == 2:
+                    elem[k] = "S"
+                elif number == 3:
+                    elem[k] = "M"
+    for i in range(FIELD_SIZE):
+        for j in range(FIELD_SIZE):
+            if i % 2 == 0 and j % 2 == 0:
+                field[i][j] = "*"
+
+    for i in range(4):
+        for j in range(4):
+            if i == 3 or j == 3:
+                field[i][j], field[FIELD_SIZE - 1 - i][FIELD_SIZE - 1 - j] = "#", "#"
+            else:
+                field[i][j], field[FIELD_SIZE - 1 - i][FIELD_SIZE - 1 - j] = ".", "."
+
+    for elem in field:
+        elem[0], elem[-1] = "*", "*"
+
+    field[0], field[-1] = ["*" * FIELD_SIZE], ["*" * FIELD_SIZE]
+
+    field[1][1], field[FIELD_SIZE - 2][FIELD_SIZE - 2] = "1", "2"
+    os.remove("data\\map.txt")
+    file = open("data\\map.txt", 'w+')
+    for elem in field:
+        file.writelines("".join(elem) + "\n")
+    file.close()
--- generate_level.py
+++ generate_level.py
@@ -1,248 +1,28 @@
-import os
+from tiles import Iron, Grass, Wood, PlayerOne, PlayerTwo, StandingEnemy, SmartEnemy, MovingEnemy
 
-import pygame
-
-size = WIDTH, HEIGHT = 950, 950
-screen = pygame.display.set_mode(size)
-
-
-#  Функция загрузки изображения
-def load_image(name, colorkey=None):
-    fullname = os.path.join("data", name)
-    image = pygame.image.load(fullname).convert()
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-#  Функция загрузки карты из текстового файла
-def load_level(filename):
-    filename = "data/" + filename
-    with open(filename, "r") as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    max_width = max(map(len, level_map))
-    mapFile.close()
-    return list(map(lambda x: x.ljust(max_width, "."), level_map))
-
-
-tile_images = {"wall": load_image("wood.png"), "empty": load_image("grass.png"),
-               "iron": load_image("iron.png")}
-player_image = load_image("mar.png", -1)
-enemy_image = load_image("bomb.png")
-bomb_image = load_image("bomb.png", -1)
-tile_width = tile_height = 50
-player = None
-tiles_grass_group = pygame.sprite.Group()
-tiles_iron_group = pygame.sprite.Group()
-tiles_box_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-enemy_group = pygame.sprite.Group()
-tiles_explosion_group = pygame.sprite.Group()
-tiles_bomb_group = pygame.sprite.Group()
-
-
-class Tile_box(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_box_group)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-
-class Tile_grass(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_grass_group)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-
-class Tile_iron(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_iron_group)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-
-class Bomb(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, player_id):
-        super().__init__(tiles_bomb_group)
-        self.image = bomb_image
-        self.rect = self.image.get_rect().move(pos_x, pos_y)
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.timer = 120
-        self.player = player_id
-        self.up_free = True
-        self.down_free = True
-        self.left_free = True
-        self.right_free = True
-
-    def update(self):
-        self.timer -= 1
-        if self.timer == 0:
-            self.explode_bomb()
-
-    def explode_bomb(self):
-        self.player.bomb_placed -= 1
-        tiles_bomb_group.remove(self)
-        explosion_sound = pygame.mixer.Sound('explosion.wav')
-        explosion_sound.play()
-        for k in range(-1 * self.player.bomb_power, self.player.bomb_power + 1):
-            for m in range(-1 * self.player.bomb_power, self.player.bomb_power + 1):
-                if k == 0 and m > 0 and self.down_free is True:
-                    if self.create_explosion(k, m):
-                        self.down_free = False
-                elif k == 0 and m < 0 and self.up_free is True:
-                    if self.create_explosion(k, m):
-                        self.down_free = False
-                elif m == 0 and k < 0 and self.left_free is True:
-                    if self.create_explosion(k, m):
-                        self.down_free = False
-                elif m == 0 and k > 0 and self.right_free is True:
-                    if self.create_explosion(k, m):
-                        self.right_free = False
-                self.create_explosion(0, 0)
-
-    def create_explosion(self, k, m):
-        explosion = Bomb_explosion(self.pos_x + k * tile_height,
-                                   self.pos_y + m * tile_height)
-        explosion.explode()
-        if explosion.intersection():
-            return True
-
-
-class Bomb_explosion(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(tiles_explosion_group)
-        self.image = load_image('boom.png')
-        self.rect = self.image.get_rect().move(pos_x, pos_y)
-        self.timer = 50
-
-    def intersection(self):
-        if pygame.sprite.spritecollide(self, tiles_box_group, False):
-            return True
-        return False
-
-    def explode(self):
-        pygame.sprite.spritecollide(self, tiles_box_group, True)
-        pygame.sprite.spritecollide(self, player_group, True)
-
-    def remove_explosion(self):
-        tiles_explosion_group.remove(self)
-
-    def update(self):
-        self.timer -= 1
-        if self.timer == 0:
-            self.remove_explosion()
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
-        self.bomb_power = 1
-        self.max_bomb_placed = 2
-        self.bomb_placed = 0
-        self.speedx = 0
-        self.speedy = 0
-
-    def place_bomb(self):
-        char_width = 24
-        char_height = 40
-        if self.bomb_placed <= self.max_bomb_placed:
-            bomb_x = (self.rect.x + char_width / 2) // tile_height * tile_height
-            bomb_y = (self.rect.y + char_height / 2) // tile_height * tile_height
-            Bomb(bomb_x, bomb_y, self)
-            self.bomb_placed += 1
-
-    def update(self):
-        self.speedx = 0
-        self.speedy = 0
-
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.speedx = -4
-        if keystate[pygame.K_RIGHT]:
-            self.speedx = 4
-        if keystate[pygame.K_DOWN]:
-            self.speedy = 4
-        if keystate[pygame.K_UP]:
-            self.speedy = -4
-        if keystate[pygame.K_SPACE]:
-            self.place_bomb()
-
-        self.rect_0 = (self.rect.x, self.rect.y)
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        if pygame.sprite.spritecollideany(self, tiles_box_group) or \
-                pygame.sprite.spritecollideany(self, tiles_iron_group):
-            self.rect.x = self.rect_0[0]
-            self.rect.y = self.rect_0[1]
-
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.top < 0:
-            self.rect.top = 0
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
-
-
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(enemy_group)
-        self.image = enemy_image
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
-        self.speedx = 0
-        self.speedy = 0
-        self.move = [-1, -1, 1, 1]
-        self.i = 0
-
-    def update(self):
-        self.speedx = 0
-        self.speedy = 0
-
-        if self.i % 4 in [0, 2]:
-            self.speedx = 3 * self.move[self.i % 4]
-        else:
-            self.speedy = 3 * self.move[self.i % 4]
-
-        self.rect_0 = (self.rect.x, self.rect.y)
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        if pygame.sprite.spritecollideany(self, tiles_box_group) or \
-                pygame.sprite.spritecollideany(self, tiles_iron_group):
-            self.rect.x = self.rect_0[0]
-            self.rect.y = self.rect_0[1]
-            self.i += 1
-
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.top < 0:
-            self.rect.top = 0
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
+enemy = []
 
 
 def generate_level_(level):
-    new_player, x, y = None, None, None
+    new_player_1, new_player_2, x, y = None, None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            Tile_grass('empty', x, y)
-            if level[y][x] == '#':
-                Tile_box('wall', x, y)
-            elif level[y][x] == '*':
-                Tile_iron("iron", x, y)
-            elif level[y][x] == '1':
-                new_player = Player(x, y)
+            Grass("grass", x, y)
+            if level[y][x] == "#":  # Wood tile creation
+                Wood("wall", x, y)
+            elif level[y][x] == "*":  # Iron tile creation
+                Iron("iron", x, y)
+            elif level[y][x] == "1":
+                new_player_1 = PlayerOne("player_1", x, y)  # Player 1 tile creation
             elif level[y][x] == "2":
-                Enemy(x, y)
-    return new_player, x, y
+                new_player_2 = PlayerTwo("player_2", x, y)  # Player 2 tile creation
+            elif level[y][x] == "E":
+                new_enemy = StandingEnemy("enemy", x, y)  # Enemy tile creation
+                enemy.append(new_enemy)
+            elif level[y][x] == "M":
+                new_moving_enemy = MovingEnemy("moving_enemy", x, y)
+                enemy.append(new_moving_enemy)
+            elif level[y][x] == "S":
+                new_smart_enemy = SmartEnemy("smart_enemy", x, y)
+                enemy.append(new_smart_enemy)
+    return new_player_1, new_player_2, enemy
